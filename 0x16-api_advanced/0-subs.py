7c3dfa5d9cef7442e2ce7556f93df2286b884b22
brian-ambani <brainambani1@gmail.com>
2023-08-25 09:55:01 +0000

    updated task-0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-"""function module"""
-import json
-import requests
+
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
+from requests import get
 
 
-def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/dev/api/r/{:s}/about".format(subreddit)
-    header = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0)\
-    Gecko/20100101 Firefox/115.0"}
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
 
-    response = requests.get(url, headers=header)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = int(data['data']['subscribers'])
-        return subscribers
-    else:
-        return 0
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
